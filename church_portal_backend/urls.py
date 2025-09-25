7bd4f2a09e5df03d9ae4605057b316ac17de29bb
Henry-ai443 <henrymaina2024@outlook.com>
2025-09-25 06:45:20 -0400

    Add root view for UptimeRobot health check

--- church_portal_backend/urls.py
+++ church_portal_backend/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from Api.views import root_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', root_view, name='root'),
     path('api/', include('Api.urls')),
 ]
 
