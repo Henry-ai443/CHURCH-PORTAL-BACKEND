71c4757da33023ab677a427843f3b510707bac0d
Henry-ai443 <henrymaina2024@outlook.com>
2025-09-29 08:50:14 -0400

    Updated profile pic upload

--- Api/admin.py
+++ Api/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Announcement, Event
-# Register your models here.
+from django import forms
+from cloudinary.forms import CloudinaryFileField
+from .models import Announcement, Event, Profile
+
+class ProfileAdminForm(forms.ModelForm):
+    profile_picture = CloudinaryFileField(
+        options = { 'folder': 'profiles' },  # optional: set your folder on Cloudinary
+        required=False,
+    )
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class ProfileAdmin(admin.ModelAdmin):
+    form = ProfileAdminForm
+
 admin.site.register(Announcement)
 admin.site.register(Event)
+admin.site.register(Profile, ProfileAdmin)
