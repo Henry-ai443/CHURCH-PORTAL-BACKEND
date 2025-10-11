05ac086cbc9b50b1008d0e0d9c33c60bef24be5e
Henry-ai443 <henrymaina2024@outlook.com>
2025-10-11 07:49:30 -0400

    Modukle error resolve

--- church_portal_backend/asgi.py
+++ church_portal_backend/asgi.py
@@ -3,7 +3,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import Api.routing  # assuming your app is called `api`
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'church_portal_backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
