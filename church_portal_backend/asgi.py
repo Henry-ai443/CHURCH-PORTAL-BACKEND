ce1ebf1378a389cd66c30c949d048be1c36bd0c5
Henry-ai443 <henrymaina2024@outlook.com>
2025-10-15 09:07:03 -0400

    Changed asgi file

--- church_portal_backend/asgi.py
+++ church_portal_backend/asgi.py
@@ -1,12 +1,14 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'church_portal_backend.settings')
+
 import django
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import Api.routing
 from Api.middleware import TokenAuthMiddleware  # your custom middleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'church_portal_backend.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
