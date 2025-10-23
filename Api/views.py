2066735cf042a60c71abb435e25bad3f6ecd64d2
Henry-ai443 <henrymaina2024@outlook.com>
2025-10-23 07:39:25 -0400

    Changed the is admin authorization

--- Api/views.py
+++ Api/views.py
@@ -358,7 +358,7 @@ class ChatMessageAPIVIEW(APIView):
 
 
 class RegisteredMembers(APIView):
-    permission_classes = [IsAdminUser, IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         users = User.objects.all().order_by('-date_joined')
